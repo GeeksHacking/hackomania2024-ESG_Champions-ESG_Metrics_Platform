172da76f7b32f826f78474ae55e302d8de50c8b4
fabiantoh98 <fabiantoh98@gmail.com>
2024-04-06 22:16:40 +0800

    added react select

--- esg_generator_app/src/components/dashboard.jsx
+++ esg_generator_app/src/components/dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend,
   CategoryScale, LinearScale, PointElement, LineElement, Title,
   BarElement
@@ -8,9 +8,63 @@ import Select from 'react-select';
 
 const Dashboard = () => {
     // first visualisation
+    const data = JSON.parse(localStorage.getItem("data"));
+    console.log(data);
     
+    const [topic, setTopic] = useState('carbon');
+    const [topicLabel, setTopicLabel] = useState('Green House Gas Emmission')
+    const [locationData, setLocationData] = useState([]);
+
+    const pieOptions = {
+      responsive: true,
+      plugins: {
+          legend: {
+              position: 'top'
+          },
+          title: {
+          display: true,
+          text: topicLabel + 'by Location',
+          },
+      },
+      };
+
+    const topicList = [
+      { "label": "Green House Gas Emmission", "value": "carbon" },
+      { "label": "Water Consumption", "value": "water" },
+      { "label": "Waste Generation", "value": "waste" },
+      { "label": "Energy Consumption", "value": "energy" },
+    ];
+
+    function handleSelectTopic(topic){
+      setTopic(topic.value);
+      setTopicLabel(topic.label);
+  }
+    function getLocationData(){
+      const topicData = data[topic];
+      setLocationData(topicData);
+    }
+
+  function handlePrint(){
+    console.log(topic);
+    console.log(locationData);
+  }
+  useEffect(() => {
+    getLocationData();
+}, [topic]);
+
     return(
-      <h1>ESG Interative Dashboard</h1>
+      <div>
+        <h1>ESG Interative Dashboard</h1>
+        <Select
+            defaultValue={topicList[0]}
+            isSearchable={true}
+            options={topicList}
+            onChange={handleSelectTopic}
+        />
+        {/* {locationData?<Pie options={pieOptions} data={locationData}/>: 'Sorry, data is not available for this filter, please try something else!'} */}
+        <button className="bg-blue-500 text-white" onClick={() => {handlePrint();}}>Print</button>
+      </div>
+      
     )
 }
 
