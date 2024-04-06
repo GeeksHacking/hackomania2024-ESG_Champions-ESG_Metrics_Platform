1d9d3681507bc4503f336b488a1b91cc743391c6
fabiantoh98 <fabiantoh98@gmail.com>
2024-04-07 03:08:55 +0800

    added line chart

--- esg_generator_app/src/components/dashboard.jsx
+++ esg_generator_app/src/components/dashboard.jsx
@@ -6,6 +6,8 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend,
 import { Pie, Line, Bar } from 'react-chartjs-2';
 import Select from 'react-select';
 
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, Title, BarElement);
+
 const Dashboard = () => {
     // first visualisation
     const data = JSON.parse(localStorage.getItem("data"));
@@ -13,7 +15,8 @@ const Dashboard = () => {
     
     const [topic, setTopic] = useState('carbon');
     const [topicLabel, setTopicLabel] = useState('Green House Gas Emmission')
-    const [locationData, setLocationData] = useState([]);
+    const [locationData, setLocationData] = useState();
+    const [periodicData, setPeriodicData] = useState();
 
     const pieOptions = {
       responsive: true,
@@ -23,11 +26,23 @@ const Dashboard = () => {
           },
           title: {
           display: true,
-          text: topicLabel + 'by Location',
+          text: topicLabel + ' by Location',
           },
       },
       };
-
+    
+      const lineOptions = {
+        responsive: true,
+        plugins: {
+          legend: {
+            display:false
+          },
+          title: {
+            display: true,
+            text: "Monthly " + topic,
+          },
+        },
+      };
     const topicList = [
       { "label": "Green House Gas Emmission", "value": "carbon" },
       { "label": "Water Consumption", "value": "water" },
@@ -51,17 +66,15 @@ const Dashboard = () => {
         return res;
       }, {});
 
-      console.log(filteredData);
-
-    if (topicData.length === 0){
-        setLocationData(null);
+    if (filteredData.length === 0){
+        setLocationData([]);
     }
     else{
       setLocationData({
         labels: filteredData.map((x) => x.site),
         datasets: [
             {
-            label: '# of Kilo Tons',
+            label: topicLabel + 'by Location',
             data: filteredData.map((x) => x.amount),
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -86,12 +99,52 @@ const Dashboard = () => {
     }
     }
 
+    function getPeriodicData(){
+      const topicData = data[topic];
+      var filteredData = [];
+      topicData.reduce(function(res, value) {
+        if (!res[value.date]) {
+          res[value.date] = { date: value.date, amount: 0 };
+          filteredData.push(res[value.date])
+        }
+        res[value.date].amount += value.amount;
+        return res;
+      }, {});
+      filteredData =filteredData.reduce((obj, item) => Object.assign(obj, { [Date.parse(item.date)]: item.amount }), {});
+      console.log(filteredData);
+      filteredData = Object.keys(filteredData).sort().reduce(
+            (obj, key) => { 
+              obj[key] = filteredData[key]; 
+              return obj;
+            }, 
+            {}
+          );
+      Object.keys(filteredData).forEach(key => {
+
+            filteredData[`${new Date(parseInt(key)).toLocaleString('default', { month: 'short' })}-${new Date(parseInt(key)).getFullYear().toString()}`] = filteredData[key];
+            delete filteredData[key];
+        });
+      console.log(filteredData);
+      setPeriodicData({
+          labels:Object.keys(filteredData),
+          datasets:[{
+          label: "Monthly " + topic,
+          data: filteredData,
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)'
+          }]
+      });
+  }
+
+
   function handlePrint(){
     console.log(topic);
     console.log(locationData);
   }
+
   useEffect(() => {
     getLocationData();
+    getPeriodicData();
 }, [topic]);
 
     return(
@@ -103,7 +156,8 @@ const Dashboard = () => {
             options={topicList}
             onChange={handleSelectTopic}
         />
-        {/* {locationData?<Pie options={pieOptions} data={locationData}/>: 'Sorry, data is not available for this filter, please try something else!'} */}
+        {locationData?<Pie options={pieOptions} data={locationData}/>: 'Sorry, data is not available for this filter, please try something else!'}
+        {periodicData? <Line options={lineOptions} data={periodicData} />: 'Sorry, data is not available for this filter, please try something else!'}
         <button className="bg-blue-500 text-white" onClick={() => {handlePrint();}}>Print</button>
       </div>
       
