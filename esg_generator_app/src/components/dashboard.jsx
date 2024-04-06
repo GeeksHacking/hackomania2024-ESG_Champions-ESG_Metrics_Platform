6ec84689fbbd4d80cadc640f4b7f9815ac6c4f76
fabiantoh98 <fabiantoh98@gmail.com>
2024-04-06 23:07:22 +0800

    pull from main

--- esg_generator_app/src/components/dashboard.jsx
+++ esg_generator_app/src/components/dashboard.jsx
@@ -41,7 +41,49 @@ const Dashboard = () => {
   }
     function getLocationData(){
       const topicData = data[topic];
-      setLocationData(topicData);
+      var filteredData = [];
+      topicData.reduce(function(res, value) {
+        if (!res[value.site]) {
+          res[value.site] = { site: value.site, amount: 0 };
+          filteredData.push(res[value.site])
+        }
+        res[value.site].amount += value.amount;
+        return res;
+      }, {});
+
+      console.log(filteredData);
+
+    if (topicData.length === 0){
+        setLocationData(null);
+    }
+    else{
+      setLocationData({
+        labels: filteredData.map((x) => x.site),
+        datasets: [
+            {
+            label: '# of Kilo Tons',
+            data: filteredData.map((x) => x.amount),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+            },
+        ],
+        });
+    }
     }
 
   function handlePrint(){
