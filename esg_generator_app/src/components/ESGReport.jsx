3da8b4994a581f4ebe80338fd9e5985237642e97
rameninc <kwang@rameninc.xyz>
2024-04-06 22:43:20 +0800

    working example of cf proxy

--- esg_generator_app/src/components/ESGReport.jsx
+++ esg_generator_app/src/components/ESGReport.jsx
@@ -4,7 +4,7 @@ const ESGReport = () => {
   const [template, setTemplate] = useState("Loading...");
 
   useEffect(() => {
-<<<<<<< HEAD
+
     const prompt ={
       contents: [
         {
@@ -27,14 +27,23 @@ const ESGReport = () => {
         method: "POST",
       }
     )
-      .then((data) => setTemplate(data))
-      .catch((error) => {
-        console.log(error);
-        alert("Error fetching template report");
-      });
-=======
-    generateReport(setTemplate);
->>>>>>> cc5fdb5a6d09cc2bb20cfbe3ded9555f9a016a3d
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      // Process your data here
+      if(data.candidates){
+        setTemplate(data.candidates[0].content.parts[0].text);
+      }
+    })
+    .catch(error => {
+      console.error('There was a problem with your fetch operation:', error);
+    });
+ //   generateReport(setTemplate);
   }, []);
 
   return (
@@ -52,37 +61,3 @@ const ESGReport = () => {
 };
 
 export default ESGReport;
-
-async function generateReport(setTemplate) {
-  const data = localStorage.getItem("data");
-  const bodyData = JSON.stringify({
-    contents: [
-      {
-        parts: [
-          {
-            text: `can you take in the following data and generate esg report like a pro? \n${data}`,
-          },
-        ],
-      },
-    ],
-  });
-  await fetch(
-    "http://localhost:8010/proxy/v1beta/models/gemini-pro:generateContent?key=AIzaSyAWsblnruBZuSzN__qUqh8oK02qgVfj_ew",
-    {
-      body: bodyData,
-      headers: {
-        apiKey: "AIzaSyAWsblnruBZuSzN__qUqh8oK02qgVfj_ew",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Methods": ["POST, GET"],
-      },
-      method: "POST",
-    }
-  ).then((response) => {
-    response.json().then((data) => {
-      const report = data.candidates[0].content.parts[0].text;
-      setTemplate(report);
-    });
-  });
-}
